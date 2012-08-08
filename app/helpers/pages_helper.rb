522ddd9ba3383c1e6d4ee646a19e30bc675dc197
Braksa Zakaria <zakaria@braksa.com>
2012-08-08 17:13:26 +0100

    added capistrano gem

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,19 +2,22 @@ module PagesHelper
 
 require "net/https"
 require 'open-uri'
+require 'openssl'
+
 
 	def asana(task,api) 
 		
 		api_key = api
 		result = JSON.parse(open("https://app.asana.com/api/1.0/workspaces", :http_basic_authentication=>[api, " "]).read)
 		workspace_id = result['data'][0]['id']
-		assignee = 'zakaria@braksa.com'
+		assignee = 'zakaria@zakaria.com'
 
 		# (source) http://developers.asana.com/documentation/ : begin
 		uri = URI.parse("https://app.asana.com/api/1.0/tasks")
 		http = Net::HTTP.new(uri.host, uri.port)
 		http.use_ssl = true
 		http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+		#http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 		header = {
 	  		"Content-Type" => "application/json"
